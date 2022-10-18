17f7f15260bbb481b8f2137d0fde9714d0209dda
oleg@powerc.ru <OlegPowerC>
2022-10-18 16:48:23 +0300

    Init

--- ipaddresses_validate_test.go
+++ ipaddresses_validate_test.go
@@ -103,3 +103,74 @@ func TestMakeListIPAddresses(t *testing.T) {
 		}
 	}
 }
+
+func TestMakeListIPAddressesWithLoginPassword(t *testing.T) {
+	IpAddrs := []string{"root:test123%a_123@192.168.0.7-10", "root:test:rrr@10.10.2.100-120", "root@172.16.0.20", "128.0.7.1-257", "128.0.71-1f", "0.240.1.7-3", "10.240.1.7_3", "10.240,1.7-3", "10.240,1. 7-3", "10.240,1.7 -3", "10.240,1.7 - 3", "10.88.1.7-10,10.88.12.8"}
+
+	IPList, RetResult := MakeListIPAddressesWithLoginPassword(IpAddrs[0])
+	if RetResult != nil {
+		t.Error("Ожидается успешое завершение, а получена ошибка:", RetResult)
+	} else {
+		if len(IPList) != 4 {
+			t.Error("Ожидается количество IP адресов 4 а получено:", len(IPList))
+		}
+		for _, rdata := range IPList {
+			if rdata.Login != "root" {
+				t.Error("Ожидается имя пользователя root а получено", rdata.Login)
+			}
+			if rdata.Password != "test123%a_123" {
+				t.Error("Ожидается пароль test123%a_123 а получен", rdata.Password)
+			}
+		}
+	}
+	IPList, RetResult = MakeListIPAddressesWithLoginPassword(IpAddrs[1])
+	if RetResult == nil {
+		t.Error("Ожидается ошибка Invalid IP user password ipaddress string")
+	}
+
+	IPList, RetResult = MakeListIPAddressesWithLoginPassword(IpAddrs[2])
+	if RetResult == nil {
+		t.Error("Ожидается ошибка Invalid IP user password ipaddress string")
+	}
+	IPList, RetResult = MakeListIPAddressesWithLoginPassword(IpAddrs[3])
+	if RetResult == nil {
+		t.Error("Ожидается ошибка")
+	}
+	IPList, RetResult = MakeListIPAddressesWithLoginPassword(IpAddrs[4])
+	if RetResult == nil {
+		t.Error("Ожидается ошибка")
+	}
+	IPList, RetResult = MakeListIPAddressesWithLoginPassword(IpAddrs[5])
+	if RetResult == nil {
+		t.Error("Ожидается ошибка")
+	}
+	IPList, RetResult = MakeListIPAddressesWithLoginPassword(IpAddrs[6])
+	if RetResult == nil {
+		t.Error("Ожидается ошибка")
+	}
+	IPList, RetResult = MakeListIPAddressesWithLoginPassword(IpAddrs[7])
+	if RetResult == nil {
+		t.Error("Ожидается ошибка")
+	}
+	IPList, RetResult = MakeListIPAddressesWithLoginPassword(IpAddrs[8])
+	if RetResult == nil {
+		t.Error("Ожидается ошибка")
+	}
+	IPList, RetResult = MakeListIPAddressesWithLoginPassword(IpAddrs[9])
+	if RetResult == nil {
+		t.Error("Ожидается ошибка")
+	}
+	IPList, RetResult = MakeListIPAddressesWithLoginPassword(IpAddrs[10])
+	if RetResult == nil {
+		t.Error("Ожидается ошибка")
+	}
+
+	IPList, RetResult = MakeListIPAddressesWithLoginPassword(IpAddrs[11])
+	if RetResult != nil {
+		t.Error("Ожидается успешое завершение, а получена ошибка:", RetResult)
+	} else {
+		if len(IPList) != 5 {
+			t.Error("Ожидается количество IP адресов 5 а получено:", len(IPList))
+		}
+	}
+}
